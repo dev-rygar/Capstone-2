9f63a63375ebb00e66f86808a3f9ca831d1b4b5a
rene24castro <151098265+rene24castro@users.noreply.github.com>
2023-12-13 22:40:57 +0800

    Added s51

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,21 +1,80 @@
-const express = require("express");
-const router = express.Router();
-const productController = require("../controllers/product")
-
+const Course = require("../models/Product");
+const bcrypt = require("bcrypt");
 const auth = require("../auth");
-const {verify, verifyAdmin} = auth;
-
-//Add a Product
-router.post("/", verify, verifyAdmin, productController.addProduct)
-
-// Retrieve all Products
-router.get("/all", verify, verifyAdmin, productController.getAllProducts);
-
-module.exports = router;
 
 
+module.exports.addProduct = (req, res) => {
+    //Validation - the product should be not existing before adding to database
+    Product.findOne({name: req.body.name})
+    .then(result => {
+        if(result){
+            return res.status(409).send({error: "Product already exist"})
+        }
+        else{
+            let newProduct = new Product({
+            name : req.body.name,
+            description: req.body.description,
+            price: req.body.price
+            })
+            return newProduct.save()
+            .then(savedProduct => res.status(201).send({savedProduct}))
+            .catch(err => {
+                console.error("Error in adding product", err)
+                return res.status(500).send({error: "Failed to add Product"})
+            })
+        }       
+    })
+}
+
+module.exports.getAllProducts = (req, res) => {
+    return Product.find({})
+    .then(products => { res.status(200).send({courses})})
+    .catch(err => res.status(500).send({error: 'Error retrieving all products'}));
+}
 
 
+
+module.exports.getProduct = (req, res) => {
+    Product.findById(req.params.productId)
+    .then(product => {
+        if(!product){
+            return res.status(404).send({error: "Product not found"})
+        }
+        else{
+            return res.status(200).send({product})
+        }
+    })
+    .catch(err => {
+        console.error("Error in retrieving the product", err);
+        return res.status(500).send({error: 'Failed to fetch product'});
+    })
+}
+
+module.exports.archiveProduct = (req, res) => {
+    return Product.findByIdAndUpdate(req.params.productId, {isActive: false}, {new: true})
+    .then(result => {
+        if(!result){
+        return res.status(404).send({error: "Product not found"});
+    }
+    else{
+        return res.status(200).send({ message: 'Product archive successfull', archiveProduct: result})
+    }
+    })
+    .catch(err => res.status(500).send({ error: 'Error finding archived product.' }));
+};
+
+module.exports.activateProduct = (req, res) => {
+    return Product.findByIdAndUpdate(req.params.productId, {isActive: true}, {new: true})
+    .then(result => {
+        if(!result){
+        return res.status(404).send({error: "Product not found"});
+    }
+    else{
+        return res.status(200).send({ message: 'Product archive successfull', activateProduct: result})
+    }
+    })
+    .catch(err => res.status(500).send({ error: 'Error finding archived product.' }));
+};
 
 
 
--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,33 +1,29 @@
-const Course = require("../models/Product");
-const bcrypt = require("bcrypt");
+const express = require("express");
+const router = express.Router();
+const productController = require("../controllers/product")
+
 const auth = require("../auth");
+const {verify, verifyAdmin} = auth;
+
+//Add a Product
+router.post("/", verify, verifyAdmin, productController.addProduct)
+
+// Retrieve all Products
+router.get("/all", verify, verifyAdmin, productController.getAllProducts);
+
+//Get a specific Product
+router.get("/:productId", productController.getAllActive);
+
+//Archiving a product
+router.patch("/:productId/archive", verify, verifyAdmin, productController.archiveProduct);
+
+//Activating an inactive Product
+ router.patch("/:productId/activate", verify, verifyAdmin, productController.archiveProduct);
 
 
-module.exports.addProduct = (req, res) => {
-    //Validation - the product should be not existing before adding to database
-    Product.findOne({name: req.body.name})
-    .then(result => {
-        if(result){
-            return res.status(409).send({error: "Product already exist"})
-        }
-        else{
-            let newProduct = new Product({
-            name : req.body.name,
-            description: req.body.description,
-            price: req.body.price
-            })
-            return newProduct.save()
-            .then(savedProduct => res.status(201).send({savedProduct}))
-            .catch(err => {
-                console.error("Error in adding product", err)
-                return res.status(500).send({error: "Failed to add Product"})
-            })
-        }       
-    })
-}
 
-module.exports.getAllProducts = (req, res) => {
-    return Product.find({})
-    .then(products => { res.status(200).send({courses})})
-    .catch(err => res.status(500).send({error: 'Error retrieving all products'}));
-}
+
+
+
+
+module.exports = router;
